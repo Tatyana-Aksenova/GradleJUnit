690fe449ff38ff3142a4465c8f59d6183e91148f
Tatyana-Aksenova <telcova@yandex.ru>
2025-07-18 16:27:46 +0300

    change yml

--- src/test/java/tests/junit5/api/tests/fakeapi/SimpleApiRefactoredTests.java
+++ src/test/java/tests/junit5/api/tests/fakeapi/SimpleApiRefactoredTests.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
-@Tag("API")
+
 @ExtendWith(RetryListenerJunit5.class)
 public class SimpleApiRefactoredTests {
     @BeforeAll
